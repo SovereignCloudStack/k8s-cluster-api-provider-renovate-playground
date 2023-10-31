fea9149570964ef4ff7e9d5bdcba3dbbfd5fc026
Danny Eiselt <eiselt@b1-systems.de>
2023-10-31 09:41:32 +0100

    Added kind to renovate
    Signed-off-by: Danny Eiselt <eiselt@b1-systems.de>

--- terraform/files/bin/install_kind.sh
+++ terraform/files/bin/install_kind.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-KIND_VERSION=0.20.0
-sudo wget -O /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/kind-linux-amd64
+KIND_VERSION=v0.19.0 # renovate: datasource=github-releases depName=kubernetes-sigs/kind
+sudo wget -O /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-linux-amd64
 sudo chmod +x /usr/local/bin/kind
 kind create cluster
 
